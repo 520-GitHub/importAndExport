f9a3a0cd1fc9d2251aca46cace521549dba34543
520-GitHub <at624258907@qq.com>
2021-05-17 10:04:55 +0800

    first commit

---
+++ src/main/java/com/example/export_import/ExportImportApplication.java
@@ -0,0 +1,13 @@
+package com.example.export_import;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExportImportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExportImportApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/export_import/ExportImportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.export_import;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExportImportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
